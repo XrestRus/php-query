68a9c74050035e789cd4a5679d4191964852bbc9
Nikita <xrestrus@gmail.com>
2021-08-02 14:38:43 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,19 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PAS', 'root');
-define('DB', 'test_posts_db');
+//Разработка
+// define('HOST', 'localhost');
+// define('USER', 'root');
+// define('PAS', 'root');
+// define('DB', 'test_posts_db');
+
+$url = parse_url(getenv("CLEARDB_CYAN_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+define('HOST', $server);
+define('USER', $username);
+define('PAS', $password);
+define('DB', $db);
